ac49d5c8c132a053864a5ef34bd5cb402437e438
arif99050 <arifulislam99050@gmail.com>
2022-05-25 15:13:49 +0600

    make footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Pages/LogIn/LogIn/LogIn';
 import Registration from './Pages/LogIn/Registration/Registration';
+import Footer from './Pages/Shared/Footer/Footer';
 import Navbar from './Pages/Shared/Header/Navbar';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
        <Route path='/registration' element={<Registration></Registration>}></Route>
      </Routes>
      
+     <Footer></Footer>
      
      
     </div>

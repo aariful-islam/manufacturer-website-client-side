b66b3e028004eeb6674a580f63f28b681f7d9c94
arif99050 <arifulislam99050@gmail.com>
2022-05-25 16:17:43 +0600

    add extra section to home

--- src/Pages/Tools/Tools.js
+++ src/Pages/Tools/Tools.js
@@ -13,7 +13,7 @@ const Tools = () => {
             <p className='text-3xl text-center'>Our Tools</p>
             <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 ml-12 gap-4'>
             {
-                tools.map(tool => <Tool
+                tools.slice(0,6).map(tool => <Tool
                     key={tool.id}
                     tool={tool}
                 ></Tool>)
